c787bd3ce17b51f8aab8ca6ba33b02166fe2d13d
JQUAN COLLINS <104362301+jquan114@users.noreply.github.com>
2023-01-31 20:18:55 -0500

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,14 @@
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+
+var canJump = function(nums) {
+         let lastPos = nums.length - 1;
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if (i + nums[i] >= lastPos) {
+            lastPos = i;
+        }
+    }
+    return lastPos === 0;
+
+};
